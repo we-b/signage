ba8c632e9dac1a509b216800385e24191c690b6a
Hiroaki WAKATSUKI <mail@puzzel.jp>
2016-11-27 15:45:40 +0900

    Add sldier

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,16 @@
-// This is where it all goes :)
+// $(function($) {
+//   $.getJSON("../data/kerning-data.json" , function(_data) {
+//     $('li').kerning({"data":_data});
+//   });
+// });
+$(document).ready(function(){
+  $('.slideshow').slick({
+    autoplay: true,
+    autoplaySpeed: 30000,
+    dots: false,
+    infinite: true,
+    fade: true,
+    arrows: false,
+    centerPadding: '0'
+  });
+});
